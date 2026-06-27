d20f4b2c3827abee4b27733ac5b4c734a5be547d
もち <motizuki225@gmail.com>
2026-06-27 19:00:52 +0900

    mottimoti_test

---
+++ Assets/_Projects/Developers/mottimoti/mottimoti_test.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class mottimoti_test : MonoBehaviour
+{
+    [SerializeField] DifficultyViewer difficultyViewer;
+    [SerializeField] string text;
+
+    void Start()
+    {
+        
+    } 
+    void Update()
+    {
+        difficultyViewer.SetDifficulty(text);
+    }
+}

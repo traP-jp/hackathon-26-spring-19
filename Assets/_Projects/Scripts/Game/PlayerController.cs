eda77b7e1b8f0b1c1b21fa2a60decbccbbaf39fb
もち <motizuki225@gmail.com>
2026-06-27 19:37:37 +0900

    PlayerController

---
+++ Assets/_Projects/Scripts/Game/PlayerController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] float minX;
+    [SerializeField] float maxX;
+    [SerializeField]bool canMove;
+
+    void Update()
+    {
+        
+    }
+
+    void Move(float inputX)
+    {
+        
+    }
+
+    void SetCanmove(bool canMove)
+    {
+        
+    }
+
+    void ClampPosition()
+    {
+        
+    }
+}
